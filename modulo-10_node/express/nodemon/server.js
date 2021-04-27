732de713fcaa009b9fced8a8ac8104251e392f39
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-04-26 23:55:11 -0300

    Iniciando express

---
+++ modulo-10_node/express/nodemon/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('Olá Mundo');
+});
+
+app.get('/contato', (req, res)=> {
+    res.send('Obrigado');
+});
+
+app.listen(3000, ()=>{
+    console.log('running on port 3000');
+    console.log('Acessar: http://localhost:3000');
+});
