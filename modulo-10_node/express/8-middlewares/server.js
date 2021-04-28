4dc2793c01221b93f1a7677ecb4ae3fa04c36465
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-04-28 01:25:31 -0300

    Middlewares

---
+++ modulo-10_node/express/8-middlewares/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const router = require('./router.js');
+const path = require('path');
+const {middlewareGlobal, outroMiddleware} = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+//Nossos middlewares
+app.use(middlewareGlobal);
+app.use(router);
+
+app.listen(3000, ()=>{
+    console.log('running on port 3000');
+    console.log('Acessar: http://localhost:3000');
+});
---
+++ modulo-10_node/express/8-middlewares/src/controllers/homeController.js
@@ -0,0 +1,13 @@
+exports.paginaInicial = (req, res)=> {
+    /* res.send(`
+    <form action="/" method="POST">
+        NOME: <input type="text" name="nome">
+        <button>Enviar</button>
+    </form>
+    `); */
+    res.render('index');
+}
+
+exports.trataPost = (req, res) => {
+    res.send(req.body);
+}
