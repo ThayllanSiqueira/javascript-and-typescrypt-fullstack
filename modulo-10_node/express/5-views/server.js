2ac24d09770ceb196fa8453894cbb4a65a765185
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-04-27 22:22:46 -0300

    Modelo Express/Webpack/Ejs

--- modulo-10_node/express/5-views/server.js
+++ modulo-10_node/express/5-views/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const router = require('./router.js');
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
 app.use(router);
 
 app.listen(3000, ()=>{
--- modulo-10_node/express/7-express-webpack/server.js
+++ modulo-10_node/express/7-express-webpack/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const router = require('./router.js');
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
 app.use(router);
 
 app.listen(3000, ()=>{
