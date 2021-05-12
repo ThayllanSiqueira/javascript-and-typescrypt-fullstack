c3d1d45adb8fc409fdad43f0af5f372eb8d95431
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-05-11 23:44:55 -0300

    Projeto Agenda do módulo 11 finalizado

---
+++ modulo-11_projeto-agenda/agenda/src/controllers/loginController.js
@@ -0,0 +1,56 @@
+const Login = require('../models/LoginModel')
+
+exports.index = (req, res) =>{
+    if(req.session.user) return res.render('login-logado');
+    res.render('login');
+}
+
+exports.register = async (req, res) =>{
+    try {
+        const login = new Login(req.body);
+        await login.register();
+
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function(){
+            return res.redirect('/login/index');
+            });
+            return;
+        }
+        req.flash('success', 'Seu usuario foi criado');
+        req.session.save(function(){
+        return res.redirect('/login/index');
+        });
+    } catch (err) {
+        console.log(err);
+        return res.render('404');
+    }
+}
+
+exports.login = async (req, res) =>{
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function(){
+            return res.redirect('/login/index');
+            });
+            return;
+        }
+        req.flash('success', 'Você entrou no sistema');
+        req.session.user = login.user;
+        req.session.save(function(){
+        return res.redirect('/login/index');
+        });
+    } catch (err) {
+        console.log(err);
+        return res.render('404');
+    }
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
