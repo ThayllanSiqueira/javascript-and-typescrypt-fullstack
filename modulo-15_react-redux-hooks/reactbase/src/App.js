396b58e25760eaf8d7396cffa6aadab60d33d34e
Thayllan Siqueira <thayllansiqueira@gmail.com>
2021-07-02 13:14:16 -0300

    Montando base react

---
+++ modulo-15_react-redux-hooks/reactbase/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <Login />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ modulo-15_react-redux-hooks/reactbase/src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={false}>
+        Login
+        <small>Oie</small>
+      </Title>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </Container>
+  );
+}
