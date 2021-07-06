ed99519a593f7813fb6ae129e45c42ac0e46c8cd
Thayllan Siqueira <thayllansiqueira@gmail.com>
2021-07-06 17:46:31 -0300

    Configurando axios

--- modulo-15_react-redux-hooks/reactbase/src/App.js
+++ modulo-15_react-redux-hooks/reactbase/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
-import Login from './pages/Login';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <Login />
+    <Router history={history}>
+      <Header />
+      <Routes />
       <GlobalStyles />
-    </>
+      <ToastContainer autoClose={3000} className="toast-container" />
+    </Router>
   );
 }
 
