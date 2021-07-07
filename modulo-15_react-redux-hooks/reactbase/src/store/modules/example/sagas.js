882ed28467259c885539356beada7bed6155efa1
Thayllan Siqueira <thayllansiqueira@gmail.com>
2021-07-07 12:38:54 -0300

    Finalização da base react

--- modulo-15_react-redux-hooks/reactbase/src/store/modules/example/sagas.js
+++ modulo-15_react-redux-hooks/reactbase/src/store/modules/example/sagas.js
@@ -1,4 +1,5 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import * as actions from './actions';
 import * as types from '../types';
 
@@ -6,7 +7,7 @@ const requisicao = () =>
   new Promise((resolve) => {
     setTimeout(() => {
       resolve();
-    }, 2000);
+    }, 600);
   });
 
 function* exampleRequest() {
@@ -14,6 +15,7 @@ function* exampleRequest() {
     yield call(requisicao);
     yield put(actions.clicaBotaoSuccess());
   } catch {
+    toast.error('Deu erro.');
     yield put(actions.clicaBotaoFailure());
   }
 }
