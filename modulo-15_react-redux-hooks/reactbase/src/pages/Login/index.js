b10b9aa97aebfa28b9f2afaae9ec6a39492256ad
Thayllan Siqueira <thayllansiqueira@gmail.com>
2021-07-06 23:43:34 -0300

    Configurando Redux, Redux Saga, Actions e Reducers

--- modulo-15_react-redux-hooks/reactbase/src/pages/Login/index.js
+++ modulo-15_react-redux-hooks/reactbase/src/pages/Login/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(exampleActions.clicaBotaoRequest());
+  }
   return (
     <Container>
       <Title isRed={false}>
@@ -11,7 +19,9 @@ export default function Login() {
         <small>Oie</small>
       </Title>
       <p>Lorem ipsum dolor sit amet consectetur.</p>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
